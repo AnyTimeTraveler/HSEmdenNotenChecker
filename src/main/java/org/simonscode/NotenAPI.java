4e7be4b5ce1ad734b4ecf7089e534b3666c74425
Simon Struck <simon@simonscode.org>
2021-04-01 14:46:11 +0200

    Initial commit

---
+++ src/main/java/org/simonscode/NotenAPI.java
@@ -0,0 +1,158 @@
+package org.simonscode;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.select.Elements;
+
+public class NotenAPI {
+
+  private Document doc;
+  private Response resp;
+  private final Map<String, String> cookies = new HashMap<>();
+
+  static String getUsernameFilename(String id) {
+    return id + ".username";
+  }
+
+  static String getGradesFilename(String username) {
+    return username + ".grades";
+  }
+
+  public boolean init(String username, String password) {
+    try {
+      HashMap<String, Double> extracted = extract(username, password);
+      write(extracted, password, new File(username + ".grades"));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+
+  public HashMap<String, Double> get_new_entries(String username)
+      throws IOException {
+
+    File file = new File(username + ".grades");
+    HashMap<String, Double> readExtract = new HashMap<>();
+    String password = read(readExtract, file);
+    HashMap<String, Double> extracted = extract(username, password);
+    write(extracted, password, file);
+    for (String key : readExtract.keySet()) {
+      extracted.remove(key);
+    }
+
+    return extracted;
+  }
+
+  private void write(HashMap<String, Double> extracted, String password, File file) throws FileNotFoundException {
+    PrintWriter pw = new PrintWriter(file);
+    pw.println(password);
+    extracted.keySet().stream().sorted().forEach(s -> pw.printf("%s:%f\n", s, extracted.get(s)));
+    pw.close();
+  }
+
+  private String read(HashMap<String, Double> extracted, File file) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    String password = null;
+    while (br.ready()) {
+      String line = br.readLine();
+      if (!line.isBlank()) {
+        if (password == null) {
+          password = line;
+        }
+        String[] parts = line.split(":");
+        if (parts.length == 2) {
+          extracted.put(parts[0], Double.parseDouble(parts[1]));
+        }
+      }
+    }
+    br.close();
+    return password;
+  }
+
+  private HashMap<String, Double> extract(String username, String password) throws IOException {
+    System.setProperty("jsse.enableSNIExtension", "false");
+
+    login(username, password);
+
+    connect(".auflistung");
+
+    connect(".liste > li:nth-child(4) > a:nth-child(1)");
+
+    connect("a.regular:nth-child(2)");
+
+    connect("ul.treelist:nth-child(4) > li:nth-child(1) > a:nth-child(3)");
+
+    Element table = doc.getElementsByTag("table").last();
+
+    Elements rows = table.getElementsByTag("tbody").first().children();
+
+    HashMap<String, Double> result = new HashMap<>();
+    for (Element row : rows) {
+      if (row.child(3).text().isBlank()) {
+        continue;
+      }
+      try {
+        result.put(row.child(1).text(), Double.parseDouble(row.child(3).text().replace(',', '.')));
+      } catch (NumberFormatException e) {
+        // do nothing
+      }
+    }
+    return result;
+  }
+
+  private void login(String username, String password) throws IOException {
+    resp = Jsoup.connect(
+        "https://sta-hisweb.hs-emden-leer.de/qisserverel/rds?state=user&type=0&topitem=")
+        .followRedirects(true)
+        .execute();
+
+    cookies.putAll(resp.cookies());
+
+    doc = resp.parse();
+    FormElement form = doc.getElementsByTag("form").forms().get(0);
+
+    form.elements().get(0).attr("value", username);
+    form.elements().get(1).attr("value", password);
+
+    resp = form.submit()
+        .cookies(cookies)
+        .referrer(resp.url().toString())
+        .followRedirects(true)
+        .method(Method.POST)
+        .execute();
+
+    cookies.putAll(resp.cookies());
+
+    doc = resp.parse();
+  }
+
+  private void connect(String cssQuery) throws IOException {
+    String href = doc.select(cssQuery).first().attr("href");
+
+    resp = Jsoup.connect(
+        href)
+        .followRedirects(true)
+        .cookies(cookies)
+        .referrer(resp.url().toString())
+        .method(Method.GET)
+        .execute();
+
+    cookies.putAll(resp.cookies());
+
+    doc = resp.parse();
+  }
+
+}
---
+++ src/main/java/org/simonscode/NotenBot.java
@@ -0,0 +1,167 @@
+package org.simonscode;
+
+import static org.simonscode.NotenAPI.getUsernameFilename;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.stream.Stream;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class NotenBot extends TelegramLongPollingBot {
+
+  public static void main(String[] args) {
+    System.setProperty("jsse.enableSNIExtension", "false");
+    try {
+      TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+      botsApi.registerBot(new NotenBot());
+    } catch (TelegramApiException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public NotenBot() {
+    Timer timer = new Timer("check-grades", true);
+    timer.scheduleAtFixedRate(
+        new TimerTask() {
+          @Override
+          public void run() {
+            try {
+              System.out.println("Checking grades...");
+              Files.list(Path.of(System.getProperty("user.dir")))
+                  .filter(p -> p.toString().endsWith(".username"))
+                  .forEach(path -> Stream.of(path)
+                      .map(p -> {
+                        try {
+                          return Files.readString(p);
+                        } catch (IOException e) {
+                          e.printStackTrace();
+                          return null;
+                        }
+                      })
+                      .filter(Objects::nonNull)
+                      .map(u -> {
+                        try {
+                          System.out.println("Checking grades for " + u + "...");
+                          return new NotenAPI().get_new_entries(u);
+                        } catch (IOException e) {
+                          e.printStackTrace();
+                          return null;
+                        }
+                      })
+                      .filter(Objects::nonNull)
+                      .filter(p -> !p.isEmpty())
+                      .forEach(changes -> {
+                        String id = path.toString().split("\\.")[0];
+                        System.out.println(id + " : " + changes);
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("Neue Note:\n");
+                        for (Entry<String, Double> entry : changes.entrySet()) {
+                          sb.append(entry.getKey());
+                          sb.append(" : ");
+                          sb.append(entry.getValue());
+                          sb.append("\n");
+                        }
+
+                        exec(new SendMessage(id, sb.toString()));
+                      }));
+              System.out.println("Checking grades: Done!");
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+        },
+        Duration.ofSeconds(5).toMillis(),
+        Duration.ofHours(1).toMillis()
+    );
+  }
+
+  @Override
+  public void onUpdateReceived(Update update) {
+    if (update.hasMessage()) {
+      Message message = update.getMessage();
+      if (message.hasText()) {
+        String text = message.getText();
+
+        if (text.startsWith("/start")) {
+          SendMessage sendMessage = new SendMessage(
+              String.valueOf(message.getChatId()),
+              "Willkommen!\n" +
+                  "Du kannst den Befehl:\n" +
+                  "  /login [benutzername] [passwort]\n" +
+                  "(ohne Klammern) verwenden,\n" +
+                  "um Benachrichtigungen von neuen Noten zu aktivieren.\n" +
+                  "\n" +
+                  "Sobald eine neue Note verfuegbar ist,\n" +
+                  "erhaelst du eine Nachricht mit der Note.\n" +
+                  "\n" +
+                  "Zum Anhalten des Bots kannst du /stop verwenden.\n"
+          );
+          exec(sendMessage);
+        }
+        if (text.startsWith("/login")) {
+          String[] parts = text.split(" ");
+
+          String reply;
+          if (new NotenAPI().init(parts[1], parts[2])) {
+            reply = "Login erfolgreich!";
+
+            try {
+              Files.writeString(Path.of(getUsernameFilename(message.getFrom().getId().toString())), parts[1]);
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+            exec(new DeleteMessage(String.valueOf(message.getChatId()), message.getMessageId()));
+          } else {
+            reply = "Login fehlgeschlagen!\nBitte melde dich bei Simon.";
+          }
+
+          exec(new SendMessage(String.valueOf(message.getChatId()), reply));
+        } else if (text.equals("/stop")) {
+          try {
+            String username = Files
+                .readString(Path.of(getUsernameFilename(message.getFrom().getId().toString())));
+            new File(NotenAPI.getGradesFilename(username)).delete();
+            Path.of(getUsernameFilename(message.getFrom().getId().toString())).toFile().delete();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    }
+  }
+
+  private <T extends Serializable> void exec(BotApiMethod<T> method) {
+    try {
+      execute(method);
+    } catch (TelegramApiException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public String getBotUsername() {
+    return "NotenBot";
+  }
+
+  @Override
+  public String getBotToken() {
+    return System.getenv("BOTTOKEN");
+  }
+}
